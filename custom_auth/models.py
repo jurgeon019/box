842f7edafa78060ccc796c299a6d3b48c684583f
andrew mendela <jurgeon018@gmail.com>
2020-02-13 17:00:49 +0200

    s

--- custom_auth/models.py
+++ custom_auth/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User, AbstractBaseUser,AbstractUser
 
 class User(AbstractUser):
   phone_number = models.CharField(verbose_name=("Номер телефону"), max_length=255, blank=True, null=True)
-
+  
   def __str__(self):
     return f"{self.first_name} {self.last_name} ({self.username}, {self.phone_number}, {self.email})"
   
