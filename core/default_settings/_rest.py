dfa1ad9b3eae9b055cfc261552c2857fd20bc9b5
andrew mendela <jurgeon018@gmail.com>
2020-04-05 13:49:12 +0300

    s

--- core/default_settings/_rest.py
+++ core/default_settings/_rest.py
@@ -12,7 +12,7 @@ SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
 }
 DJOSER = {
-    'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
+    'PASSWORD_RESET_CONFIRM_URL': 'auth/password/reset/confirm/{uid}/{token}',
     'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
     'ACTIVATION_URL': '#/activate/{uid}/{token}',
     'SEND_ACTIVATION_EMAIL': True,
--- core/sw_auth/urls.py
+++ core/sw_auth/urls.py
@@ -2,7 +2,14 @@ from django.urls import path, include
 from .views import * 
 
 
+
+def confirm(request):
+    return render(request, 'core/confirm.html', locals())
+
+
 urlpatterns = [
     path('', include('box.core.sw_auth.api.urls')),
-    path('sw_logout/', sw_logout, name='sw_logout')
+    path('sw_logout/', sw_logout, name='sw_logout'),
+    path('confirm/<uid>/<token>/', confirm),
+
 ]
