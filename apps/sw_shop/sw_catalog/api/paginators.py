e611b01beaf934576201955b7fc5e42e8f85ed5d
andrew mendela <jurgeon018@gmail.com>
2020-04-28 22:29:27 +0300

    s

--- apps/sw_shop/sw_catalog/api/paginators.py
+++ apps/sw_shop/sw_catalog/api/paginators.py
@@ -2,7 +2,7 @@ from rest_framework.pagination import BasePagination, LimitOffsetPagination, Pag
 
 
 class StandardPageNumberPagination(PageNumberPagination):
-    page_size              = 5
+    page_size              = 6
     max_page_size          = 1000
     page_query_param       = 'page_number'
     page_size_query_param  = 'per_page'
